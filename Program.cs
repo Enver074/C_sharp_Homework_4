7823bcccb368f1bcc066481427cb27ec30830430
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-12 10:15:00 +0300

    Первое задание

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите число А");
+int numbA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число В");
+int numbB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Число {numbA} в степени {numbB} = {Exponentation(numbA)}");
+
+int Exponentation(int number){
+    int Exp = Convert.ToInt32(Math.Pow(numbA,numbB));
+    return Exp;
+}
