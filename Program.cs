8110eace22fe2305e23df18e8fa87c9204e22419
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-12 18:55:48 +0300

    Третье задание

--- Program.cs
+++ Program.cs
@@ -29,16 +29,34 @@
 // 9012 -> 12
 
 
-Console.WriteLine("Введите число: ");
-int numb = Convert.ToInt32(Console.ReadLine()!);
+// Console.WriteLine("Введите число: ");
+// int numb = Convert.ToInt32(Console.ReadLine()!);
 
-Console.WriteLine(GetSum(numb));
+// Console.WriteLine(GetSum(numb));
 
-int GetSum(int i){
-    int sum = 0;
-    while(i > 0){
-    sum += i % 10;
-    i /= 10;}
-    return sum;
-    }    
-        
+// int GetSum(int i){
+//     int sum = 0;
+//     while(i > 0){
+//     sum += i % 10;
+//     i /= 10;}
+//     return sum;
+//     }  
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] array = new int[8];
+GetBinnaryarray(array.Length);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+void GetBinnaryarray(int size){
+    for(int i = 0; i < size; i++){
+    array[i] = new Random().Next(0,100);
+   }
+}
+
